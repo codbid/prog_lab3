f07892dd85ea32a57e8abe6aa4f1829033f9add1
Даниил Истомин <istomin.05.3@gmail.com>
2023-04-30 19:20:49 +0700

    Some commit v2

--- ex2/main.c
+++ ex2/main.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
     char example2[] = "decode";
     if ( argc != 4 || ( strcmp(argv[1], example1) != 0 && strcmp(argv[1], example2) != 0) )
     {
-        printf("Usage:\ncoder encode <in-file-name> <out-file-name>\n");
+        printf("Usage:\ncoder <encode/decode> <in-file-name> <out-file-name>\n");
         return -1;
     }
     if ( !strcmp(argv[1], example1) )
